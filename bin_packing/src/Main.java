478cdb918a37bef5dbfe8919e581185a01193883
Dena Alharbi <109204661+DenaAlharbi@users.noreply.github.com>
2023-09-16 16:54:54 +0300

    Update Main.java

--- bin_packing/src/Main.java
+++ bin_packing/src/Main.java
@@ -6,49 +6,39 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        Scanner input= new Scanner(System.in);
+       Scanner input = new Scanner(System.in);
         System.out.println("Enter the number of objects: ");
-        int numContainers =input.nextInt();
+        int numContainers = input.nextInt();
         ArrayList<Integer> originalarray = new ArrayList<Integer>();
-
         System.out.println("Enter the weights: ");
-        for(int i=0;i<numContainers;i++) {
-            originalarray.add(input.nextInt()) ;
+        for (int i = 0; i < numContainers; i++) {
+            int user = input.nextInt();
+            originalarray.add(user);
         }
-        input.close();
-        //int numOfBins=0;
-		/*for(int i=0;i<originalarray.length;i++) {
-		System.out.println(originalarray[i]);
-		}*/
-        ArrayList<Integer> container_current_weights = new ArrayList<Integer>();
-        //this should run unti the entered values are all removed
+        ArrayList<ArrayList<Integer>> allContainers = new ArrayList<ArrayList<Integer>>();
         while (!originalarray.isEmpty()) {
-            //go through it one by one
-            for(int i=0;i<originalarray.size();i++) {
-                //go through each container to check if it can pack the new weight
-                for(int j=0;j<container_current_weights.size();j++) {
-                    //getting the sum in order to check availability
-                    int[] sums = new int[container_current_weights.size()];
-                    if(sums[j]+ originalarray.get(i)<10) {
-                        //Integer num = originalarray.get(i); ignore
-                        //if yes-it should add the weight to the container and remove it from the original
-                        container_current_weights.get(j).add((int)originalarray.get(i));
-                        originalarray.remove(i);
-                    }
-                    //if all containers are full a new one is added
-                    if(j==container_current_weights.size()) {
-                        //this should create a new container
-                        //idk if this would work (maybe it needs a new array?) i cant check cause the code stops at line35
-                        container_current_weights.add(originalarray.get(i));
-
-                    }
+            int tot = 0; // works better than the sums method i did before and much simpiler
+            int num; // to save the value from remove(this is the solution to the main issue i had before)
+            ArrayList<Integer> weightInTheContainer = new ArrayList<Integer>();
+            for (int i = 0; i < originalarray.size(); i++) {
+                if ((tot + originalarray.get(i)) < 10) {
+                    //Integer num = originalarray.get(i); ignore
+                    //if yes-it should add the weight to the container and remove it from the original
+                    //container_current_weights.get(j).add((int)originalarray.get(i));
+                    num = originalarray.remove(i);
+                    i--;
+                    weightInTheContainer.add(num);
+                    tot += num;
                 }
             }
+            allContainers.add(weightInTheContainer);
         }
         //just print statements
-        System.out.println("Number of containers needed: " + container_current_weights.size());
-        for (int i = 0; i < container_current_weights.size(); i++) {
-            System.out.println("Container " + (i + 1) + ": " + container_current_weights.get(i));
+        System.out.println("Number of containers needed: " + allContainers.size());
+        for (int i = 0; i < allContainers.size(); i++) {
+            System.out.println("Container " + (i + 1) + ": " + allContainers.get(i));
+
+
         }
     }
 }
